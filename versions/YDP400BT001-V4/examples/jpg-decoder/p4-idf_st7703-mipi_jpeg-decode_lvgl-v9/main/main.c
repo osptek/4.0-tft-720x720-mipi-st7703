30c8a49945e87fa3d8a765b1ed979d31de8c516f
BLSNCD <2857585364@qq.com>
2026-08-17 09:38:03 +0800

    docs(examples): add OSPTEK SPDX header to example entry main.c

--- versions/YDP400BT001-V4/examples/jpg-decoder/p4-idf_st7703-mipi_jpeg-decode_lvgl-v9/main/main.c
+++ versions/YDP400BT001-V4/examples/jpg-decoder/p4-idf_st7703-mipi_jpeg-decode_lvgl-v9/main/main.c
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 OSPTEK
+ * SPDX-License-Identifier: CC-BY-4.0
+ *
+ * https://github.com/osptek
+ */
+
 #include "lvgl_init.h"
 #include "esp_sdmmc_card.h"
 #include "esp_jpeg_decode.h"
